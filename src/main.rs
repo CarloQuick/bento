69ae6a4168bfda80b4ce6cc902168e5b99f4f185
CarloQuick <98628846+CarloQuick@users.noreply.github.com>
2025-09-18 19:51:36 +0900

    dev(cli): Added Create basics to cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+// use nix::sys::utsname::uname;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates a container without running it
+    Create {
+        /// Image for writable container layer
+        #[arg(short, long)]
+        image: String,
+        /// Name of the runnable container
+        #[arg(short, long)]
+        name: Option<String>,
+    },
+}
+
+fn _bento_cli() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Create { image, name }) => {
+            println!("Image to pull: {}", image);
+            if let Some(name) = name {
+                println!("Container name: {name}")
+            }
+        }
+        None => {}
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    _bento_cli();
 }
