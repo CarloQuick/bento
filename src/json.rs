076a70826889151784c947852ceae695805fa953
CarloQuick <98628846+CarloQuick@users.noreply.github.com>
2025-10-10 19:31:22 +0900

    dev(extract): Extracting docker image, creating directory for container, and making bento_config.json

--- src/json.rs
+++ src/json.rs
@@ -1,3 +1,10 @@
+use colored::Colorize;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, to_writer_pretty};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Write};
+use std::path::{Path, PathBuf};
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct IncomingJson {
     architecture: String,
@@ -7,57 +14,17 @@ pub struct IncomingJson {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct IncomingConfig {
-    #[serde(rename = "Hostname")]
-    hostname: Option<String>,
-    #[serde(rename = "Domainname")]
-    domainname: Option<String>,
-    #[serde(rename = "User")]
-    user: Option<String>,
-    #[serde(rename = "AttachStdin")]
-    attach_stdin: Option<bool>,
-    #[serde(rename = "AttachStdout")]
-    attach_stdout: Option<bool>,
-    #[serde(rename = "AttachStderr")]
-    attach_stderr: Option<bool>,
-    #[serde(rename = "Tty")]
-    tty: Option<bool>,
-    #[serde(rename = "OpenStdin")]
-    open_stdin: Option<bool>,
-    #[serde(rename = "StdinOnce")]
-    stdin_once: Option<bool>,
     #[serde(rename = "Env")]
     env: Vec<String>,
     #[serde(rename = "Cmd")]
     cmd: Vec<String>,
-    #[serde(rename = "Image")]
-    image: Option<String>,
-    #[serde(rename = "Volumes")]
-    volumes: Option<()>,
-    #[serde(rename = "WorkingDir")]
-    working_dir: Option<String>,
-    #[serde(rename = "Entrypoint")]
-    entrypoint: Option<()>,
-    #[serde(rename = "OnBuild")]
-    on_build: Option<()>,
-    #[serde(rename = "Labels")]
-    labels: Option<Labels>,
 }
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Labels {
-    #[serde(rename = "org.opencontainers.image.ref.name")]
-    org_opencontainers_image_ref_name: String,
-    #[serde(rename = "org.opencontainers.image.version")]
-    org_opencontainers_image_version: String,
-}
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RootFs {
     #[serde(rename = "type")]
     fs_type: String,
     diff_ids: Vec<String>,
 }
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BentoConfigJson {
     architecture: String,
@@ -79,21 +46,17 @@ impl BentoConfigJson {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct IndexJson {
-    #[serde(rename = "schemaVersion")]
-    schema_version: u32,
     #[serde(rename = "mediaType")]
     media_type: String,
-    manifests: Vec<IndexManifest>,
+    manifests: Vec<Manifests>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct IndexManifest {
+pub struct Manifests {
     #[serde(rename = "mediaType")]
     media_type: String,
     digest: String,
     platform: Option<Platform>,
-    size: u32,
-    annotations: Annotations,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -101,18 +64,9 @@ pub struct Platform {
     architecture: String,
     os: String,
 }
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Annotations {
-    #[serde(rename = "io.containerd.image.name")]
-    io_containerd_image_name: String,
-    #[serde(rename = "org.opencontainers.image.ref.name")]
-    org_opencontainers_image_ref_name: String,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ManifestJson {
-    #[serde(rename = "schemaVersion")]
-    schema_version: i32,
     #[serde(rename = "mediaType")]
     media_type: String,
     config: ManifestConfig,
@@ -123,7 +77,6 @@ pub struct ManifestConfig {
     #[serde(rename = "mediaType")]
     media_type: String,
     digest: String,
-    size: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -131,7 +84,6 @@ pub struct ManifestLayers {
     #[serde(rename = "mediaType")]
     media_type: String,
     digest: String,
-    size: i32,
 }
 
 fn create_bento_json<P: AsRef<Path>>(read_path: P, write_path: P) -> Result<()> {
@@ -172,9 +124,8 @@ fn get_config_path(digest: &str) -> Option<PathBuf> {
     }
 }
 
-fn get_index_json(mut cont_path: PathBuf) -> Result<IndexJson> {
-    cont_path.push("index.json");
-    let file = File::open(&cont_path).expect("Couldnt open Index.json");
+fn get_index_json(index_json_path: &PathBuf) -> Result<IndexJson> {
+    let file = File::open(index_json_path).expect("Couldnt open Index.json");
     let reader = BufReader::new(file);
     // Read the JSON contents of the file as an instance of `Address`.
     let a: IndexJson = serde_json::from_reader(reader)?;
@@ -182,7 +133,6 @@ fn get_index_json(mut cont_path: PathBuf) -> Result<IndexJson> {
 }
 
 fn get_manifest_json(manifest_path: &PathBuf) -> Result<ManifestJson> {
-    // cont_path.push("index.json");
     let file = File::open(&manifest_path).expect("Couldnt open Index.json");
     let reader = BufReader::new(file);
     // Read the JSON contents of the file as an instance of `Address`.
@@ -190,27 +140,74 @@ fn get_manifest_json(manifest_path: &PathBuf) -> Result<ManifestJson> {
     Ok(a)
 }
 
-///Users/c_quick/dev/containers/test_images/python
-fn read_write_json() {
-    let container_str = String::from("/Users/c_quick/dev/containers/test_images/python");
-    let write_path: PathBuf =
-        PathBuf::from("/Users/c_quick/dev/containers/test_images/python/bento_config.json");
-    let cont_path: PathBuf = PathBuf::from(&container_str);
-    let index_json = get_index_json(cont_path).expect("Could not read from index.json");
-    let manifest_path = get_config_path(&index_json.manifests[0].digest);
-    match manifest_path {
-        None => panic!("No config"),
-        Some(manifest) => {
-            let please_remove = PathBuf::from(&container_str).join(&manifest);
-            let manifest_json =
-                get_manifest_json(&please_remove).expect("Couldnt get the manifest.json");
-            let config_path = get_config_path(&manifest_json.config.digest);
-            match config_path {
-                None => panic!("No config"),
-                Some(bento_config) => {
-                    let please_remove = PathBuf::from(&container_str).join(&bento_config);
-                    create_bento_json(please_remove, write_path)
-                        .expect("Failed to create bento json");
+fn get_nested_manifest(
+    tmp_path: &PathBuf,
+    nested_index_json_path: &Option<PathBuf>,
+) -> Option<usize> {
+    if let Some(nested_path) = nested_index_json_path {
+        let nested_json =
+            get_index_json(&tmp_path.join(nested_path)).expect("Failed to get nested JSON");
+        for (i, manifest) in nested_json.manifests.iter().enumerate() {
+            if let Some(platform_arch) = &manifest.platform {
+                if platform_arch.architecture == "amd64" {
+                    return Some(i);
+                }
+            }
+        }
+    }
+    None
+}
+
+// TESTING .bento/containers/python
+pub fn read_write_json(cont_path: PathBuf) {
+    // it writes config to bento container path
+    let write_path: PathBuf = PathBuf::from(&cont_path).join("bento_config.json");
+    eprintln!("write_path {:?}\n", write_path);
+    // the untarred images path at tmp
+    let tmp_path: PathBuf = PathBuf::from(&cont_path).join("tmp");
+    eprintln!("tmp_path {:?}\n", tmp_path);
+    // blobs | index.json | manifest.json | oci-layout
+    let index_json_path: PathBuf = PathBuf::from(&tmp_path).join("index.json");
+    eprintln!("index_json_path {:?}\n", index_json_path);
+    let index_json = get_index_json(&index_json_path).expect("Could not read from index.json");
+    eprintln!("index_json {:?}\n", index_json);
+    if index_json.manifests[0].media_type.contains("image.index") {
+        eprintln!(
+            "index_json.manifests[0].media_type {:?}\n",
+            index_json.manifests[0].media_type
+        );
+        let nested_digest = &index_json.manifests[0].digest;
+        eprintln!("nested_digest {:?}\n", nested_digest);
+        let nested_index_json_path = get_config_path(nested_digest);
+        eprintln!("nested_index_json_path {:?}\n", nested_index_json_path);
+
+        if let Some(nested_path) = &nested_index_json_path {
+            let amd64_manifest = get_nested_manifest(&tmp_path, &nested_index_json_path);
+            if let Some(index) = amd64_manifest {
+                // now that we have an index we want the nested index.json
+                let nested_json =
+                    get_index_json(&tmp_path.join(nested_path)).expect("Failed to get nested JSON");
+                let manifest_path = get_config_path(&nested_json.manifests[index].digest);
+                eprintln!("manifest_path: {:?}", manifest_path);
+
+                match manifest_path {
+                    None => panic!("No config"),
+                    Some(manifest) => {
+                        let please_remove = PathBuf::from(&tmp_path).join(&manifest);
+                        eprintln!("please_remove: {:?}", please_remove);
+
+                        let manifest_json = get_manifest_json(&please_remove)
+                            .expect("Couldnt get the manifest.json");
+                        let config_path = get_config_path(&manifest_json.config.digest);
+                        match config_path {
+                            None => panic!("No config"),
+                            Some(bento_config) => {
+                                let please_remove = PathBuf::from(&tmp_path).join(&bento_config);
+                                create_bento_json(please_remove, write_path)
+                                    .expect("Failed to create bento json");
+                            }
+                        }
+                    }
                 }
             }
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cli;
 pub mod extract;
+pub mod json;
 pub mod runtime;
